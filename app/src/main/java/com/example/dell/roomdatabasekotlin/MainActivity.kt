4a4925cbc8af582e370e3b208aa6e5db4d5343a0
sdiksha09 <diksha.sharma1209@gmail.com>
2018-08-28 18:31:55 +0530

    first commit

--- app/src/main/java/com/example/dell/roomdatabasekotlin/MainActivity.kt
+++ app/src/main/java/com/example/dell/roomdatabasekotlin/MainActivity.kt
@@ -1,10 +1,17 @@
 package com.example.dell.roomdatabasekotlin
 
+import android.content.DialogInterface
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
+import android.text.TextUtils
+import android.view.ContextMenu
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.EditText
 import android.widget.Toast
 import com.example.dell.roomdatabasekotlin.Model.User
 import io.reactivex.disposables.CompositeDisposable
@@ -13,17 +20,21 @@ import com.example.dell.roomdatabasekotlin.Database.UserRepository
 import com.example.dell.roomdatabasekotlin.Local.UserDatabase
 import com.example.dell.roomdatabasekotlin.Local.UserDataSource
 import com.example.dell.roomdatabasekotlin.Local.UserDataSource.Companion
+import io.reactivex.Observable
+import io.reactivex.ObservableOnSubscribe
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.functions.Action
 import io.reactivex.schedulers.Schedulers
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter:ArrayAdapter<*>
-    var userList:MutableList<User> = ArrayList() //A generic ordered collection of elements that supports adding and removing elements.
-                                                // Parameters User - the type of elements contained in the list. The mutable list is invariant on its element type.
+    lateinit var adapter: ArrayAdapter<*>
+    var userList: MutableList<User> = ArrayList() //A generic ordered collection of elements that supports adding and removing elements.
+    // Parameters User - the type of elements contained in the list. The mutable list is invariant on its element type.
     //Database
-    private var compositeDisposable:CompositeDisposable?=null
-    private var userRepository:UserRepository?=null
+    private var compositeDisposable: CompositeDisposable? = null
+    private var userRepository: UserRepository? = null
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +45,7 @@ class MainActivity : AppCompatActivity() {
 
         compositeDisposable = CompositeDisposable()
 
-        adapter = ArrayAdapter(this,android.R.layout.simple_list_item_2,userList)  // simple_list_item_2 has two inside a subclass of RelativeLayout
+        adapter = ArrayAdapter(this, android.R.layout.simple_list_item_2, userList)  // simple_list_item_2 has two inside a subclass of RelativeLayout
         registerForContextMenu(user_lst)
         user_lst!!.adapter = adapter
 
@@ -49,16 +60,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun loadData() {
-        val disposible=userRepository!!.allusers
+        val disposible = userRepository!!.allusers
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribeOn(Schedulers.io())
-                .subscribe({users-> onGetAllUsersSuccess(users)}){
+                .subscribe({ users -> onGetAllUsersSuccess(users) }) {
 
-                    Throwable-> Toast.makeText(this@MainActivity,""+Throwable.message,Toast.LENGTH_SHORT).show()
+                    Throwable ->
+                    Toast.makeText(this@MainActivity, "" + Throwable.message, Toast.LENGTH_SHORT).show()
                 }
         compositeDisposable!!.add(disposible)
-
-        
 
 
     }
@@ -72,20 +82,81 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate(R.menu.main_menu,menu)
+        menuInflater.inflate(R.menu.main_menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        when(item.itemId){
+        when (item!!.itemId) {
 
-            R.id.clear->deleteAllUser()
+            R.id.clear -> deleteAllUser()
 
         }
         return super.onOptionsItemSelected(item)
     }
 
     private fun deleteAllUser() {
-        val disposable
+        val disposable = Observable.create(ObservableOnSubscribe<Any> { e ->
+            userRepository!!.deleteAllUsers()
+            e.onComplete()
+        })
+
+                .observeOn(AndroidSchedulers.mainThread())
+                .observeOn(Schedulers.io())
+                .subscribe(io.reactivex.functions.Consumer { },
+                        io.reactivex.functions.Consumer {
+
+                            Throwable ->
+                            Toast.makeText(this@MainActivity, "" + Throwable.message, Toast.LENGTH_SHORT).show()
+
+                        },
+                        Action { loadData() }
+                )
+
+        compositeDisposable!!.addAll(disposable)
+    }
+
+    override fun onCreateContextMenu(menu: ContextMenu?, v: View?, menuInfo: ContextMenu.ContextMenuInfo?) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+
+        val info = menuInfo as AdapterView.AdapterContextMenuInfo
+        menu!!.setHeaderTitle("Select Action")
+
+        menu.add(Menu.NONE, 0, Menu.NONE, "Update")
+        menu.add(Menu.NONE, 1, Menu.NONE, "Delete")
+    }
+
+    override fun onContextItemSelected(item: MenuItem?): Boolean {
+        return super.onContextItemSelected(item)
+
+        val info = item!!.menuInfo as AdapterView.AdapterContextMenuInfo
+        val user = userList[info.position]
+
+        when (item.itemId) {
+            0 -> {
+                val edtName = EditText(this@MainActivity)
+                edtName.setText(user.name)
+                edtName.hint = "Enter your Name"
+
+                //Create Dialog
+
+                AlertDialog.Builder(this@MainActivity)
+                        .setTitle("Edit")
+                        .setMessage("Edit Your Name")
+                        .setView(edtName)
+                        .setPositiveButton(android.R.string.ok, DialogInterface.OnClickListener { dialog, which ->
+                            if (TextUtils.isEmpty(edtName.text.toString()))
+                                return@OnClickListener
+                            else {
+
+                                user.name = edtName.text.toString()
+                                updateUser(user)
+                            }
+                        }).setNegativeButton(android.R.string.cancel)
+
+            }
+        }
+
+
     }
 }
